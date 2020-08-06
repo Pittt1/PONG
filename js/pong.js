99b9bf3c3319176990bcce0d299549ee74ee123e
Piotr Sławek <pslawek@sevroll.system>
2020-08-06 12:57:19 +0200

    Prepared the window in Canvas js file and styles in css

---
+++ js/pong.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+/*** WINDOW SIZE ***/
+
+canvas.width = 900;
+canvas.height = 600;
+cw = canvas.width;
+ch = canvas.height;
+
+/*** WINDOW SIZE ***/
